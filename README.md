2392f945c58edb6c80bd6acca53edcc9c92e5e2a
KimsPractice <tpghksdldia@naver.com>
2020-01-13 15:15:17 +0900

    setup server

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GuessMind
+
 Realtime Drawing Game built with SocketIO, Gulp, NodeJS
+
+## To Do
+
+- [x] Server
+- [x] Pug
+- [ ] Static
+- [ ] SocketIO
--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("hello");
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "pug");
+app.set("views", "src/views");
+app.use(express.static("src/static"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
